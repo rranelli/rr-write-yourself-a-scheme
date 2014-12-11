e8243f90d87e59d5346b59ad1b0c0ca296d55d83
renan-ranelli <renanranelli@gmail.com>
2014-12-10 22:40:26 -0200

    More stuff on chapter 2

--- chapter2/simpleparser.hs
+++ chapter2/simpleparser.hs
@@ -2,6 +2,10 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
+import Numeric
+
+-- exercises
+-- 1x,2x,3x,4o,5x,6,7
 
 main :: IO ()
 main = do arg : _ <- getArgs
@@ -11,6 +15,8 @@ data LispVal = Atom String
              | List [LispVal]
              | DottedList [LispVal] LispVal
              | Number Integer
+             | Float Float
+             | Char Char
              | String String
              | Bool Bool
 
@@ -25,6 +31,11 @@ readExpr = \input -> case parse parseExpr "lisp" input of
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseCharacter :: Parser LispVal
+parseCharacter = do string "#\\"
+                    x <- anyChar
+                    return $ Char x
+
 parseString :: Parser LispVal
 parseString = do char '"'
                  x <- many (noneOf "\"")
@@ -38,12 +49,42 @@ parseAtom = do first <- letter <|> symbol
                return $ case atom of
                           "#t" -> Bool True
                           "#f" -> Bool False
-                          otherwise -> Atom atom
+                          _ -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = (many1 digit) >>= (\x -> return $ Number (read x))
+-- parseNumber = do x <- (many1 digit)
+--                  return $ Number (read x)
+
+-- parseFloat :: Parser LispVal
+-- parseFloat = do whole <- (many1 digit)
+--                 char '.'
+--                 frac <- (many1 digit)
+--                 let num = whole ++ "." ++ frac
+--                 return $ case (readFloat num) of
+--                           [] -> []
+--                           [(x, _)] -> Float x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do head <- endBy parseExpr spaces
+                     tail <- char '.' >> spaces >> parseExpr
+                     return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do char '\''
+                 x <- parseExpr
+                 return $ List [Atom "quote", x]
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
+        <|> parseCharacter
         <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- (try parseList) <|> parseDottedList
+               char ')'
+               return x
