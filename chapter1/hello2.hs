05db07ffb656a9890ad1694c967dec3209b52f7e
renan-ranelli <renanranelli@gmail.com>
2014-12-24 02:03:08 -0200

    Remove old chapter structure

--- chapter1/hello2.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-import System.Environment
-
-main :: IO ()
-main = do args <- getArgs
-          putStrLn ("Sum of your stuff is " ++ show ((read $ args !! 0) + (read $ args !! 1)))
--- chapter1/hello3.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-import System.Environment
-
-main :: IO ()
-main = do name <- getLine -- this whole thing here is a shorthand for monadic sequencing
-          putStrLn $ "Hello, " ++ name
