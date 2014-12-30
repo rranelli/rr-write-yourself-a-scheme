b6ab123561848744fce89ef5ac79367067f950ca
renan-ranelli <renanranelli@gmail.com>
2014-12-30 02:28:06 -0200

    Extract `Repl` to `SchemeRepl.hs`.

---
+++ SchemeRepl.hs
@@ -0,0 +1,30 @@
+{-#LANGUAGE DoAndIfThenElse #-}
+module SchemeRepl (runRepl, evalAndPrint) where
+
+import System.IO
+import Control.Monad.Except
+
+import SchemeError
+import SchemeReader
+import SchemeEvaluator
+
+flushStr :: String -> IO ()
+flushStr = \str -> putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt = \prompt -> flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString = \expr -> return . extractValue . trapError $ (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint = \expr -> evalString expr >>= putStrLn
+
+until_ :: (String -> Bool) -> IO String -> (String -> IO ()) -> IO ()
+until_ haltp prompt action = do result <- prompt
+                                if haltp result
+                                then return ()
+                                else (action result) >> until_ haltp prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp >>> ") evalAndPrint
